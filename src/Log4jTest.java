6bbb1edba45c705a54134a2771ec5bf55b781acc
SecondMountain <1256809441@qq.com>
2016-09-27 16:38:18 +0800

    log4j recode log

---
+++ src/Log4jTest.java
@@ -0,0 +1,30 @@
+import org.apache.log4j.Logger;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static Print.Print.println;
+
+/**
+ * Created by zyf on 2016/9/27.
+ */
+public class Log4jTest {
+    public static Logger logger = Logger.getLogger(Regex.class);
+    public static void main(String[] args) {
+        //TODO write your code here
+        Pattern pattern = Pattern.compile("[0-9]");
+        String str="zhofjdioa464654";
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()){
+            println(matcher.group());
+//            matcher.replaceAll(" ");全部替换为。。。
+            logger.debug("This is debug message.");
+            // 记录info级别的信息
+            logger.info("This is info message.");
+            // 记录error级别的信息
+            logger.error("This is error message.");
+            logger.warn("This is warn message");
+
+        }
+    }
+}
