fae5d7983444c281bd1806aaa3501d667359c25e
SecondMountain <1256809441@qq.com>
2016-09-27 16:36:48 +0800

    modify some code

--- src/Regex.java
+++ src/Regex.java
@@ -1,3 +1,5 @@
+import org.apache.log4j.Logger;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -7,6 +9,7 @@ import static Print.Print.println;
  * Created by zyf on 2016/3/1.
  */
 public class Regex {
+    public static Logger logger = Logger.getLogger(Regex.class);
     public static void main(String[] args) {
         //TODO write your code here
         Pattern pattern = Pattern.compile("[0-9]");
@@ -15,6 +18,13 @@ public class Regex {
         while (matcher.find()){
             println(matcher.group());
 //            matcher.replaceAll(" ");全部替换为。。。
+            logger.debug("This is debug message.");
+          // 记录info级别的信息
+            logger.info("This is info message.");
+             // 记录error级别的信息
+            logger.error("This is error message.");
+            logger.warn("This is warn message");
+
         }
     }
 }
